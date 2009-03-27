720ffc165c57bb344b00ce63dc2e0ac6b610c6c7
David Cantrell <david@cantrell.org.uk>
2009-03-27 12:39:05 +0000

    Fix schema, add dbish

--- schema.sql
+++ schema.sql
@@ -2,19 +2,19 @@ CREATE TABLE modules (
     module      VARCHAR(256),
     modversion  VARCHAR(24),
     dist        VARCHAR(256),
-    distversion VARCHAR(24),
+    distversion VARCHAR(24)
 );
-CREATE UNIQUE INDEX modules_idx packages(module, modversion, dist, distversion);
+CREATE UNIQUE INDEX modules_idx ON modules(module, modversion, dist, distversion);
 CREATE TABLE dists (
     dist        VARCHAR(256),
     distversion VARCHAR(24),
     file        VARCHAR(256)
 );
-CREATE UNIQUE INDEX dists_idx dists(dist, distversion);
-CREATE UNIQUE INDEX files_idx dists(file);
+CREATE UNIQUE INDEX dists_idx ON dists(dist, distversion);
+CREATE UNIQUE INDEX files_idx ON dists(file);
 CREATE TABLE passes (
     dist        VARCHAR(256),
     distversion VARCHAR(24),
     perl        VARCHAR(8)
 );
-CREATE        INDEX perl_idx passes(perl);
+CREATE        INDEX perl_idx ON passes(perl);
