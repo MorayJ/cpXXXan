bc1d0487966f460a679df1a2a5bb4cb9f1be4d2e
David Cantrell <david@cantrell.org.uk>
2009-03-30 15:48:29 +0100

    Prepare for deployment ...

--- build02packages.pl
+++ build02packages.pl
@@ -5,11 +5,20 @@ use strict;
 
 use DBI;
 use Data::Dumper;
+use HTTP::Date;
+
+# Configuration for DRC's laptop and for live
+use constant BACKPAN => -e '/web/cpxxxan/backpan'
+    ? '/web/cpxxxan/backpan'
+    : '/Users/david/BackPAN';
+use constant CPXXXANROOT => -e '/web/cpxxxan'
+    ? '/web/cpxxxan'
+    : '.';
 
 my $perl = shift();
 die("Must specify a perl, eg\n\n  \$ $0 5.6.2\n") unless($perl);
 
-my $cpxxxan = DBI->connect('dbi:SQLite:dbname=db/cpXXXan');
+my $cpxxxan = DBI->connect('dbi:SQLite:dbname='.CPXXXANROOT.'/db/cpXXXan');
 
 my $data = $cpxxxan->selectall_arrayref(qq{
     SELECT module, modversion, d.dist, d.distversion, d.file
@@ -30,15 +39,26 @@ my $data = $cpxxxan->selectall_arrayref(qq{
            )
   ORDER BY module
 }, {Slice => {}});
-# print Dumper($data);
-# exit(0);
+
+mkdir CPXXXANROOT."/cp${perl}an";
+mkdir CPXXXANROOT."/cp${perl}an/modules";
+mkdir CPXXXANROOT."/cp${perl}an/authors";
+symlink BACKPAN."/authors/01mailrc.txt.gz",
+     CPXXXANROOT."/cp${perl}an/authors/01mailrc.txt.gz";
+symlink BACKPAN."/modules/03modlist.data.gz",
+     CPXXXANROOT."/cp${perl}an/modules/03modlist.data.gz";
+symlink BACKPAN."/authors/id",
+     CPXXXANROOT."/cp${perl}an/authors/id";
 
 open(my $packagesfile, '>', "cp${perl}an/modules/02packages.details.txt")
     || die("Can't write cp${perl}an/modules/02packages.details.txt\n");
-print $packagesfile "This is a whitespace-seperated file.\n";
-print $packagesfile "Each line is modulename moduleversion filename.\n";
+print $packagesfile "Description: This is a whitespace-seperated file.\n";
+print $packagesfile "Description: Each line is modulename moduleversion filename.\n";
+print $packagesfile "Line-Count: ".@{$data}."\n";
+print $packagesfile "Last-Updated: ".HTTP::Date::time2str()."\n";
 print $packagesfile "\n";
 print $packagesfile sprintf(
-    "%s\t%s\t%s\n", $_->{module}, $_->{modversion}, $_->{'file'}
+    "%s %s %s\n", $_->{module}, $_->{modversion}, $_->{'file'}
 ) foreach (@{$data});
 close($packagesfile);
+system(qw(gzip -9f), "cp${perl}an/modules/02packages.details.txt");
