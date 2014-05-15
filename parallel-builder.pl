5822f3d695ebe95b1cff8f45dd7a26c1c88064f0
David Cantrell <david@cantrell.org.uk>
2014-05-15 09:08:48 +0100

    run slowest jobs first, for MOAR EFFISHUNTSY

--- parallel-builder.pl
+++ parallel-builder.pl
@@ -3,21 +3,32 @@
 use strict;
 use warnings;
 use Parallel::ForkManager;
+use DBM::Deep;
 
 my $build_script = shift(@ARGV);
 
 my $par = Parallel::ForkManager->new(2);
+my $db  = DBM::Deep->new(
+    file => "/tmp/parallel-builder-history.db",
+);
+
+# order jobs by how long they took last time (assuming 0
+# seconds if not seen before), longest first
+my @jobs = sort {
+    (exists($db->{$b}) ? $db->{$b} : 0) <=>
+    (exists($db->{$a}) ? $db->{$a} : 0)
+} map { s/'//g; $_ } @ARGV;
 
 print "start: ".localtime()."\n";
 my $start = time();
-foreach my $arg (@ARGV) {
-  $arg =~ s/'//g;
+foreach my $job (@jobs) {
   $par->start() && next;
 
-  my $output = "$arg: ".localtime()." - ";
+  my $output = "$job: ".localtime()." - ";
   my $start = time();
-  system("$build_script $arg");
-  print "  $output".localtime()."; secs: ".(time() - $start)."\n";
+  system("$build_script $job");
+  $db->{$job} = time() - $start;
+  print "  $output".localtime()."; secs: ".$db->{$job}."\n";
   $par->finish();
 }
 $par->wait_all_children();
